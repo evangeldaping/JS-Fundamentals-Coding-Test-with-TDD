1c1f9ed18a098a5c41f5fff5d1f4cd83f4e5ac95
evangel <evangeldaping@gmail.com>
2021-11-04 20:52:30 +0800

    using for loop

--- src/bigArrSum.js
+++ src/bigArrSum.js
@@ -8,7 +8,77 @@ const arr = [1, 2, 2, 3, 1, 4, 3, 5];
 
 export const sumArr = (arr) => {
 	// Code here
-	return 0;
+	// Using for loop
+	let sum = 0;
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i+1] > arr[i]) {
+			sum += arr[i+1]
+		}
+	}
+	return sum;
 };
 
 console.log(sumArr(arr));
+
+// first loop
+// variable   			value
+// i          			0
+// arr[i+1]   			[0+1]=>[1]=>2
+// arr[i]     			[0]=>1
+// arr[i+1] > arr[i]    2>1 => true
+// sum += arr[i+1]      0+2
+// sum					2
+
+// second loop
+// variable   			value
+// i          			1
+// arr[i+1]   			[1+1]=>[2]=>2
+// arr[i]     			[1]=>2
+// arr[i+1] > arr[i]    2>2 => false
+// sum += arr[i+1]      
+// sum					2
+
+// third loop
+// variable   			value
+// i          			2
+// arr[i+1]   			[2+1]=>[3]=>3
+// arr[i]     			[2]=>2
+// arr[i+1] > arr[i]    3>2 => true
+// sum += arr[i+1]      2+3
+// sum					5
+
+// fourth loop
+// variable   			value
+// i          			3
+// arr[i+1]   			[3+1]=>[4]=>1
+// arr[i]     			[3]=>3
+// arr[i+1] > arr[i]    1>3 => false
+// sum += arr[i+1]      
+// sum					5
+
+// fifth loop
+// variable   			value
+// i          			4
+// arr[i+1]   			[4+1]=>[5]=>4
+// arr[i]     			[4]=>1
+// arr[i+1] > arr[i]    4>1 => true
+// sum += arr[i+1]      5+4
+// sum					9
+
+// sixth loop
+// variable   			value
+// i          			5
+// arr[i+1]   			[5+1]=>[6]=>3
+// arr[i]     			[5]=>4
+// arr[i+1] > arr[i]    3>4 => false
+// sum += arr[i+1]      
+// sum					9
+
+// seventh loop
+// variable   			value
+// i          			6
+// arr[i+1]   			[6+1]=>[7]=>5
+// arr[i]     			[6]=>3
+// arr[i+1] > arr[i]    5>3 => true
+// sum += arr[i+1]      9+5
+// sum					14
