9a9d91f6520ce83e30ce1208efeb08757e4604bb
evangel <evangeldaping@gmail.com>
2021-11-04 21:17:11 +0800

    using for loop

--- src/bigArrSumDiff.js
+++ src/bigArrSumDiff.js
@@ -8,7 +8,93 @@ const arr = [1, 2, 2, 3, 1, 4, 3, 5]; // 8
 
 export const sumArrDiff = (arr) => {
 	// Code here
-	return 0;
+	let sumDiff = 0;
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i+1] > arr[i]) {
+			sumDiff += arr[i+1]
+		}
+		else if (arr[i+1] <= arr[i]) {
+			sumDiff -= arr[i+1]
+		}
+	}
+	return sumDiff;
 };
 
 console.log(sumArrDiff(arr));
+
+// first loop
+// variable   			value
+// i          			0
+// arr[i+1]   			[0+1]=>[1]=>2
+// arr[i]     			[0]=>1
+// arr[i+1] > arr[i]    2>1 => true
+// sumDiff += arr[i+1]  0+2
+// arr[i+1] <= arr[i]   2 <= 1 => false
+// sumDiff -= arr[i+1]  
+// sumDiff				2
+
+// second loop
+// variable   			value
+// i          			1
+// arr[i+1]   			[1+1]=>[2]=>2
+// arr[i]     			[1]=>2
+// arr[i+1] > arr[i]    2>2 => false
+// sumDiff += arr[i+1]
+// arr[i+1] <= arr[i]   2 <= 2 => true
+// sumDiff -= arr[i+1]  2-2
+// sumDiff				0
+
+// third loop
+// variable   			value
+// i          			2
+// arr[i+1]   			[2+1]=>[3]=>3
+// arr[i]     			[2]=>2
+// arr[i+1] > arr[i]    3>2 => true
+// sumDiff += arr[i+1]  0+3
+// arr[i+1] <= arr[i]   3 <= 2 => false
+// sumDiff -= arr[i+1]  
+// sumDiff				3
+
+// fourth loop
+// variable   			value
+// i          			3
+// arr[i+1]   			[3+1]=>[4]=>1
+// arr[i]     			[3]=>3
+// arr[i+1] > arr[i]    1>3 => false
+// sumDiff += arr[i+1]
+// arr[i+1] <= arr[i]   1 <= 3 => true
+// sumDiff -= arr[i+1]  3-1
+// sumDiff				2
+
+// fifth loop
+// variable   			value
+// i          			4
+// arr[i+1]   			[4+1]=>[5]=>4
+// arr[i]     			[4]=>1
+// arr[i+1] > arr[i]    4>1 => true
+// sumDiff += arr[i+1]  2+4
+// arr[i+1] <= arr[i]   4 <= 1 => false
+// sumDiff -= arr[i+1]  
+// sumDiff				6
+
+// sixth loop
+// variable   			value
+// i          			5
+// arr[i+1]   			[5+1]=>[6]=>3
+// arr[i]     			[5]=>4
+// arr[i+1] > arr[i]    3>4 => false
+// sumDiff += arr[i+1]
+// arr[i+1] <= arr[i]   3 <= 4 => true
+// sumDiff -= arr[i+1]  6-3
+// sumDiff				3
+
+// seventh loop
+// variable   			value
+// i          			6
+// arr[i+1]   			[6+1]=>[7]=>5
+// arr[i]     			[6]=>3
+// arr[i+1] > arr[i]    5>3 => true
+// sumDiff += arr[i+1]  3+5
+// arr[i+1] <= arr[i]   5 <= 3 => false
+// sumDiff -= arr[i+1]  
+// sumDiff				8
