f21671d31d8398407be77002790cd0ee8debeecb
evangel <evangeldaping@gmail.com>
2021-11-10 00:15:25 +0800

    using for loop and filter

--- src/arrNot.js
+++ src/arrNot.js
@@ -4,14 +4,37 @@
 // arr1 = [1, 2, 3, 4, 5, 6];
 // arr2 = [1, 2, 5, 6, 7];
 
-// Output : [3, 5, 7]
+// Output : [3, 4, 7]
 
 const arr1 = [1, 2, 3, 4, 5, 6];
 const arr2 = [1, 2, 5, 6, 7];
 
-export const arrNotFunc = (arr1, arr2) => {
+// Using for loop
+export const arrNotFuncFor = (arr1, arr2) => {
 	// Code here
-	return 0;
+	const uniqueArr = [];
+	for (let i = 0; i < arr1.length; i++) {
+		if (arr2.indexOf(arr1[i]) === -1) {
+			uniqueArr.push(arr1[i]);
+		}
+	}
+	for (let i = 0; i < arr2.length; i++) {
+		if (arr1.indexOf(arr2[i]) === -1) {
+			uniqueArr.push(arr2[i]);
+		}
+	}
+	return uniqueArr;
 };
+
+console.log(arrNotFuncFor(arr1, arr2));
+
+// Using filter
+// https://stackoverflow.com/questions/1187518/how-to-get-the-difference-between-two-arrays-in-javascript
+export const arrNotFunc = (arr1, arr2) => {
+	const arr1Filtered = arr1.filter(el => !arr2.includes(el));
+	const arr2Filtered = arr2.filter(el => !arr1.includes(el));
+	
+	return [...arr1Filtered, ...arr2Filtered]
+}
 
 console.log(arrNotFunc(arr1, arr2));
